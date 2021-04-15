84939a245dd5b6f1902a71a28ed9253845147463
Susan Trinh <snt.trinh@gmail.com>
2021-04-15 13:36:10 -0600

    Customer is able to delete on main window

--- src/controllers/CustomerController.java
+++ src/controllers/CustomerController.java
@@ -15,9 +15,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -27,6 +25,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -56,13 +55,12 @@ public class CustomerController {
     private Button deleteButton; // Value injected by FXMLLoader
 
 
-
     int selectedIndex;
-    Customer selectedCustomer = new Customer() ;
+    Customer selectedCustomer = new Customer();
 
 
-
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+        // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         tableViewCustomers.getSelectionModel().selectFirst();
 
@@ -81,22 +79,22 @@ public class CustomerController {
         tableViewCustomers.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                if(t1.intValue()>=0){
-                    selectedIndex =  t1.intValue();
+                if (t1.intValue() >= 0) {
+                    selectedIndex = t1.intValue();
                 }
 
             }
         });
 
-            tableViewCustomers.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Customer>() {
-                @Override
-                public void changed(ObservableValue<? extends Customer> observableValue, Customer customer, Customer t1) {
+        tableViewCustomers.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Customer>() {
+            @Override
+            public void changed(ObservableValue<? extends Customer> observableValue, Customer customer, Customer t1) {
 
-                    if(t1!=null){
+                if (t1 != null) {
 //                        System.out.println("customer : "+t1);
 
-                        editButton.disableProperty().setValue(false);
-                        btnViewBookings.disableProperty().setValue(false);
+                    editButton.disableProperty().setValue(false);
+                    btnViewBookings.disableProperty().setValue(false);
 //                        selectedCustomer.setCustomerId(t1.getCustomerId());
 //                        selectedCustomer.setCustFirstName(t1.getCustFirstName());
 //                        selectedCustomer.setCustLastName(t1.getCustLastName());
@@ -110,31 +108,31 @@ public class CustomerController {
 //                        selectedCustomer.setCustEmail(t1.getCustEmail());
 //                        selectedCustomer.setAgentId(t1.getAgentId());
 
-                    selectedCustomer = new Customer(t1.getCustomerId(),t1.getCustFirstName(),t1.getCustLastName(),t1.getCustAddress(),
-                            t1.getCustCity(),t1.getCustProv(),t1.getCustPostal(),t1.getCustCountry(),t1.getCustHomePhone(),t1.getCustBusPhone(),
-                            t1.getCustEmail(),t1.getAgentId());
-                    }
+                    selectedCustomer = new Customer(t1.getCustomerId(), t1.getCustFirstName(), t1.getCustLastName(), t1.getCustAddress(),
+                            t1.getCustCity(), t1.getCustProv(), t1.getCustPostal(), t1.getCustCountry(), t1.getCustHomePhone(), t1.getCustBusPhone(),
+                            t1.getCustEmail(), t1.getAgentId());
                 }
-            });
+            }
+        });
 
 
-            btnViewBookings.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent actionEvent) {
-                    try {
-                        FXMLLoader fxmlLoader = new FXMLLoader();
-                        fxmlLoader.setLocation(getClass().getResource("../layout/customerBookings.fxml"));
-                        /*
-                         * if "fx:controller" is not set in fxml
-                         * fxmlLoader.setController(NewWindowController);
-                         */
-                        fxmlLoader.setController(new CustomerBookingsController(selectedCustomer.getCustomerId()));
-                        Scene scene = new Scene(fxmlLoader.load());
-                        Stage stage = new Stage();
-                        stage.setTitle("Customer Bookings");
-                        stage.setScene(scene);
-                        stage.initModality(Modality.APPLICATION_MODAL);
-                        stage.show();
+        btnViewBookings.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                try {
+                    FXMLLoader fxmlLoader = new FXMLLoader();
+                    fxmlLoader.setLocation(getClass().getResource("../layout/customerBookings.fxml"));
+                    /*
+                     * if "fx:controller" is not set in fxml
+                     * fxmlLoader.setController(NewWindowController);
+                     */
+                    fxmlLoader.setController(new CustomerBookingsController(selectedCustomer.getCustomerId()));
+                    Scene scene = new Scene(fxmlLoader.load());
+                    Stage stage = new Stage();
+                    stage.setTitle("Customer Bookings");
+                    stage.setScene(scene);
+                    stage.initModality(Modality.APPLICATION_MODAL);
+                    stage.show();
 
 
 //                        stage.setOnHiding(new EventHandler<WindowEvent>() {
@@ -145,48 +143,48 @@ public class CustomerController {
 //                            }
 //                        });
 
-                    } catch (IOException e) {
-                        Logger logger = Logger.getLogger(getClass().getName());
-                        logger.log(Level.SEVERE, "Failed to create new Window.", e);
-                    }
+                } catch (IOException e) {
+                    Logger logger = Logger.getLogger(getClass().getName());
+                    logger.log(Level.SEVERE, "Failed to create new Window.", e);
                 }
-            });
-            editButton.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent actionEvent) {
-                    try {
-                        FXMLLoader fxmlLoader = new FXMLLoader();
-                        fxmlLoader.setLocation(getClass().getResource("../layout/customersEdit.fxml"));
-                        /*
-                         * if "fx:controller" is not set in fxml
-                         * fxmlLoader.setController(NewWindowController);
-                         */
-                        fxmlLoader.setController(new CustomerEditController(selectedCustomer));
-                        Scene scene = new Scene(fxmlLoader.load());
-                        Stage stage = new Stage();
-                        stage.setTitle("Edit Customer");
-                        stage.setScene(scene);
-                        stage.initModality(Modality.APPLICATION_MODAL);
-                        stage.show();
+            }
+        });
+        editButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                try {
+                    FXMLLoader fxmlLoader = new FXMLLoader();
+                    fxmlLoader.setLocation(getClass().getResource("../layout/customersEdit.fxml"));
+                    /*
+                     * if "fx:controller" is not set in fxml
+                     * fxmlLoader.setController(NewWindowController);
+                     */
+                    fxmlLoader.setController(new CustomerEditController(selectedCustomer));
+                    Scene scene = new Scene(fxmlLoader.load());
+                    Stage stage = new Stage();
+                    stage.setTitle("Edit Customer");
+                    stage.setScene(scene);
+                    stage.initModality(Modality.APPLICATION_MODAL);
+                    stage.show();
 
 
-                        stage.setOnHiding(new EventHandler<WindowEvent>() {
-                            @Override
-                            public void handle(WindowEvent windowEvent) {
-                                fillCustomerTable();
-                                tableViewCustomers.getSelectionModel().select(selectedIndex);
+                    stage.setOnHiding(new EventHandler<WindowEvent>() {
+                        @Override
+                        public void handle(WindowEvent windowEvent) {
+                            fillCustomerTable();
+                            tableViewCustomers.getSelectionModel().select(selectedIndex);
 
-                            }
-                        });
+                        }
+                    });
 
-                    } catch (IOException e) {
-                        Logger logger = Logger.getLogger(getClass().getName());
-                        logger.log(Level.SEVERE, "Failed to create new Window.", e);
-                    }
-
-
+                } catch (IOException e) {
+                    Logger logger = Logger.getLogger(getClass().getName());
+                    logger.log(Level.SEVERE, "Failed to create new Window.", e);
                 }
-            });
+
+
+            }
+        });
         addButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
@@ -220,6 +218,41 @@ public class CustomerController {
             }
         });
 
+        deleteButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                //Customer cust = new Customer(tableViewCustomers.getSelectionModel().getSelectedIndex());
+                System.out.println(tableViewCustomers.getSelectionModel().getSelectedIndex());
+
+                Alert alert = new Alert(Alert.AlertType.WARNING,"Are you Sure, you want to delete this Customer?",ButtonType.CANCEL,ButtonType.YES);
+                Optional<ButtonType> result =  alert.showAndWait();
+
+                Customer customer = tableViewCustomers.getSelectionModel().getSelectedItem();
+                System.out.println(customer.getCustomerId());
+
+                if(result.get() == ButtonType.YES){
+                    try {
+                        MySQLConnectionData MySQL = new MySQLConnectionData();
+                        Connection conn = MySQL.getMySQLConnection();
+                        Statement stmt = conn.createStatement();
+
+                        String sql = "Delete FROM customers where CustomerId = "+customer.getCustomerId();
+                        stmt.executeUpdate(sql);
+
+                        Alert alert2 = new Alert(Alert.AlertType.CONFIRMATION, "Customer has been deleted", ButtonType.OK);
+                        conn.close();
+
+//                        alert2.show();
+//                        Stage stage = (Stage) btnSaveCustomer.getScene().getWindow();
+//                        stage.close();
+
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    }
+                }
+            }
+        });
+
     }
 
     private void fillCustomerTable() {
@@ -238,8 +271,8 @@ public class CustomerController {
             while (rsCustomers.next()) {
                 customerList.add(new Customer(rsCustomers.getInt(1), rsCustomers.getString(2), rsCustomers.getString(3),
                         rsCustomers.getString(4), rsCustomers.getString(5), rsCustomers.getString(6),
-                        rsCustomers.getString(7),rsCustomers.getString(8),rsCustomers.getString(9),
-                        rsCustomers.getString(10),rsCustomers.getString(11),rsCustomers.getInt(12)));
+                        rsCustomers.getString(7), rsCustomers.getString(8), rsCustomers.getString(9),
+                        rsCustomers.getString(10), rsCustomers.getString(11), rsCustomers.getInt(12)));
                 listOfCustomers.add(rsCustomers.getString(2));
             }
             rsCustomers.next();
@@ -249,7 +282,7 @@ public class CustomerController {
             TableColumn temp;
             tableViewCustomers.getColumns().clear();
 
-            for(int i=1;i<=colCount;i++){
+            for (int i = 1; i <= colCount; i++) {
                 temp = new TableColumn();
                 temp.setText(rsmd.getColumnLabel(i));
                 temp.setCellValueFactory(new PropertyValueFactory<>(rsmd.getColumnLabel(i)));
